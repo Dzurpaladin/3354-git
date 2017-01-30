8a62cf06dddaebd00c3c03b64878fc16467fa0a2
RodolfoJancan <rxj122130@utdallas.edu>
2017-01-29 18:27:51 -0600

    Modififed Adder Introduced subtraction into adder class via character comparision if first char is '-' then it adds second and third number and multiplies by -1

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,9 +1,18 @@
-package adder;
+/**
+ * @(#)Main.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/1/29
+ */
 
-public class Main {
-
-    public static void main(String[] args) {
-        try {
+public class Main
+	{
+    public static void main(String[] args)
+    	{
+   
+        try
+        {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
@@ -11,7 +20,14 @@ public class Main {
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
+    private static int addArguments(String[] realArgs) {
+    	
+    	if(args[0].charAt(0)=='-')
+    		{
+ 			return ((Integer.valueOf(args[1])+Integer.valueOf(args[2])) *(-1));
+    		}
+    	else
+        	return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
     }
 }
+
